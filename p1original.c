3ef52fd3d1efa73d105861b41ea86666e369e85e
himanshusharm52 <97221111+himanshusharma652@users.noreply.github.com>
2022-03-10 10:14:21 +0000

    p3original.c

--- p1original.c
+++ p1original.c
@@ -1,2 +1,8 @@
-
+#include<stdio.h>
+int main()
+{
+  int num;
+ printf("himanshu");
+  scanf("%d",num);
+ return 0;
 }
---
+++ p3original.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int input_array_size()
+{
+   int n;
+   printf("enter an array size\n");
+   scanf("%d"&n);
+   return 0;
+}
+
+void input_array(int n,int a[n]);
+{
+  for (i=0,i<n,i++);
+  printf("enter the value of array\n");
+    scanf("%d",int n,int a[n]);
+  
+}
+int sum_composite_numbers(int n,int a[n])
+void out_put(int sum)
+{
+  int sum=0;
+  for (j=0,j<n,j++)
+}
+if(c>i)
+   sum=sum=a[n];
+return c;
+
